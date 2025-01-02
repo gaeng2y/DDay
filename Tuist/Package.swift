c5395225a0ac1c8c857f376f867e4c9f86a56cea
gaeng2y <gaeng2y@gmail.com>
2025-01-02 23:51:11 +0900

    ➕ #10 Package 의존성 추가
    - RxSwift, ReactorKit, RIBs 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,5 +18,8 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", branch: "main"),
+        .package(url: "https://github.com/ReactorKit/ReactorKit.git", branch: "master"),
+        .package(url: "https://github.com/uber/RIBs-iOS.git", branch: "main")
     ]
 )
